0e7a498ea6b15edf45c67e1b61752ca2405d2d3f
Pjmaciel <pjmaciel_4@hotmail.com>
2025-02-20 16:39:04 -0300

    feat(logout): implemented session logout.

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,12 @@
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
+import { onMounted } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.initialize(); // Configura o cabeçalho de autorização ao carregar a aplicação
+});
 </script>
